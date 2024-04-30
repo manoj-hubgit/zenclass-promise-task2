bac1401804cc3e8088626726a72b82da01e56c54
manoj-hubgit <manojannadurai2265@gmail.com>
2024-04-30 16:47:14 +0530

    files uploaded

---
+++ script.js
@@ -0,0 +1,45 @@
+//accessing the input and button.
+const buttons = document.getElementsByClassName("button")[0];
+const amount = document.getElementsByClassName("amount")[0];
+const code = document.getElementsByClassName("code")[0];
+//the function will start after the button is clicked.
+buttons.addEventListener("click", function () {
+  //if the input field is empty it will show alert.
+  if (amount.value === "" || code.value === "") {
+    alert("enter the valid input");
+  }
+   //if the input value is not empty
+  else {
+    //using fetch to get data from api.
+    //the api have template litrals to get currency code value.
+    const response = fetch(
+      `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${code.value}.json`
+    );
+    response
+      .then((data) => data.json()) 
+      .then((result) => {
+        //if we get usd in code.value .It will get the inr value in api for usd
+        let indrupees = result[code.value].inr;
+        let rupees = (indrupees * amount.value).toFixed(2);
+        //whenever we get the result the search button will be disappear.
+        buttons.style.display = "none";
+        // console.log(rupees);
+        //creating a h2 to display the amount in rupees
+        let div = document.createElement("div");
+        let para = document.createElement("h2");
+        para.innerHTML = `${amount.value} ${code.value} is ${rupees} Rupees`;
+        div.classList = "result ms-2 align-content-sm-center";
+        let refresh = document.createElement("button");
+        refresh.innerText = "Reset";
+        refresh.classList = "btn btn-outline-primary ms-2 reset";
+        refresh.type = "button";
+        div.append(para, refresh);
+        document.body.append(div);
+        //To reset used windows refresh to reset.
+        const reset = document.getElementsByClassName("reset")[0];
+        reset.addEventListener("click", () => {
+          window.location.reload();
+        });
+      });
+  }
+});
